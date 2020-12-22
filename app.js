733938909bad4ebbce4689cebd1f05821980c5df
Christopher-John Cassar <christopherjcassar@gmail.com>
2020-12-22 23:11:26 +0000

    - Added some routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const port = 3000;
 
 const app = express();
 
@@ -16,8 +17,20 @@ app.use(bodyParser.urlencoded({
 
 
 
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
